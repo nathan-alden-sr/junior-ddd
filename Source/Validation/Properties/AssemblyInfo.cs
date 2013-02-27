a52187382edec09872cb33c386dba41c0986ebc7
Nathan Alden <visualcsharpcoder@hotmail.com>
2013-02-26 20:46:25 -0600

    Now targeting .NET Framework 4.5 Updated JuniorCommon to 3.0.0

--- Source/Validation/Properties/AssemblyInfo.cs
+++ Source/Validation/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Runtime.InteropServices;
 
 [assembly:ComVisible(false)]
 [assembly:AssemblyTitle("Junior.Ddd.Validation")]
-[assembly:AssemblyVersion("2.0.0.0")]
-[assembly:AssemblyFileVersion("2.0.0.0")]
+[assembly:AssemblyVersion("3.0.0.0")]
+[assembly:AssemblyFileVersion("3.0.0.0")]
